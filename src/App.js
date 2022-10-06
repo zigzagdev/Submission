7792de152228920c3b09c914016238cce0a7add2
zigzagdev <msts.oo0131@gmail.com>
2022-10-06 16:37:24 +0900

    TopPage_Add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import {Routes, BrowserRouter, Route} from 'react-router-dom';
+import Top from  './main/Top'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="Wrapper">
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<Top/>}/>
+              </Routes>
+          </BrowserRouter>
+      </div>
   );
 }
 
---
+++ src/main/Top.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function Top () {
+    return(
+        <div>
+            <strong>sasfa</strong>
+        </div>
+    )
+}
+export default Top;
