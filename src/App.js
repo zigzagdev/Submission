f861c592bcd5dd3a219b047696619ab8aab631f7
zigzagdev <msts.oo0131@gmail.com>
2022-10-07 21:37:39 +0900

    fix_mainbranch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import './App.css';
-import React, { Component } from 'react';
-import {Routes, BrowserRouter, Route} from 'react-router-dom';
-import Top from  './main/Top'
 
 function App() {
   return (
-      <div className="Wrapper">
-          <BrowserRouter>
-              <Routes>
-                  <Route path="/" element={<Top/>}/>
-              </Routes>
-          </BrowserRouter>
-      </div>
+
   );
 }
 
