899099cd9e525fec3db7c1e9498597a8c8a740cc
zigzagdev <msts.oo0131@gmail.com>
2022-10-07 21:39:37 +0900

    fix_TopPage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import React, { Component } from 'react';
+import {Routes, BrowserRouter, Route} from 'react-router-dom';
+import Top from  './main/Top'
 
 function App() {
   return (
-
+      <div className="Wrapper">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Top/>}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
