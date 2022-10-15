0f66daab3f0568728fee6d86ef8ff838cbf40d0b
zigzagdev <msts.oo0131@gmail.com>
2022-10-16 01:03:19 +0900

    fix_toastcontent

--- src/components/toast.js
+++ src/components/toast.js
@@ -1,1 +1,18 @@
-import React from "react";
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Toast() {
+    const showToastMessage = () => {
+        toast.success('Success Notification !', {
+            position: toast.POSITION.TOP_RIGHT
+        });
+    };
+    return (
+        <div>
+            <button onClick={showToastMessage}>Notify</button>
+            <ToastContainer />
+        </div>
+    );
+}
+export default Toast;
