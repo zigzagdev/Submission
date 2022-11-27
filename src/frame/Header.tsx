88b264e05829b27a65b8ab55c5b355ef237c916d
zigzagdev <msts.oo0131@gmail.com>
2022-11-27 17:50:33 +0900

    header_blockFix

--- src/frame/Header.tsx
+++ src/frame/Header.tsx
@@ -1,14 +1,39 @@
 import * as React from 'react';
 
-const outline: React.CSSProperties  = {
-    display: "float",
-    margin: "60px 0 31px 0",
-}
+const outLine: { [key: string]: string } = {
+    display: "flex",
+    margin: "60px 0 31px 30px",
+};
 
+const eachBox: { [key: string]: string } = {
+    display: "inline-block",
+    marginLeft: "50px",
+    fontSize: "30px",
+};
+const aa: { [key: string]: string } = {
+    width:"250px",
+};
+
+const right: { [key: string]: string } = {
+    display: "block",
+    marginLeft: '1000px'
+};
+const right2: { [key: string]: string } = {
+    display: "block",
+    marginLeft: '30px'
+};
 const Header = () => {
     return(
-        <div style ={outline} >
-          aaa
+        <div style={outLine}>
+            <div style={eachBox}>
+                left side
+            </div>
+            <div style={right}>
+                right side
+            </div>
+            <div style={right2}>
+                right side2
+            </div>
         </div>
     )
 }
