c573472255539fb29d8739b571c4f50c871afa0c
zigzagdev <msts.oo0131@gmail.com>
2023-02-06 21:51:32 +0900

    fixIndent

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,98 @@
-
-
-
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
 
-
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host : 'localhost',
-    port : 'root',
-    user : 'root',
-    database: 'submission'
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "root",
+    database: "react",
 });
 
-app.get("/", function(req, res) {
-    res.json("test clear !");
+app.post("/create", (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+
+    db.query(
+        "INSERT INTO employees (name, age, country) VALUES (?,?,?)",
+        [name, age, country],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send("Values Inserted");
+            }
+        }
+    );
 });
 
-app.listen(4000, function() {
-    console.log("Example app listening on port 4000!");
+app.get("/opinions", (req, res) => {
+    db.query("SELECT * FROM opinions", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
 });
+
+app.put("/update", (req, res) => {
+    const id = req.body.id;
+    const country = req.body.country;
+    db.query(
+        "UPDATE employees SET country = ? WHERE id = ?",
+        [country, id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+app.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM opinions WHERE id = ?", id, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.listen(3001, () => {
+    console.log("Yey, your server is running on port 3001");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
