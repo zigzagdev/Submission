5175d6a08597643d45ff19cc831ea465695c38ad
zigzagdev <msts.oo0131@gmail.com>
2023-02-11 14:45:13 +0900

    HeaderMaking_complete

--- frontend/src/frame/Header.js
+++ frontend/src/frame/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
+import {Button} from "@mui/material";
 
 const outline = {
     display:'flex',
     justifyContent:'space-between',
     height: 100,
     backgroundColor: 'snow',
-    textAlign: "right",
     minWidth:'1024px',
 }
 
@@ -24,11 +24,53 @@ const style2 = {
     lineHeight:3,
 
 }
+const right = {
+    display: "block",
+};
+const right2 = {
+    display: "block",
+    margin: '30px 10px'
+};
+
+const headerSame = {
+    display: "flex",
+    margin: "0 40px 0 auto",
+};
+
+const sameLine = {
+    display: "flex",
+    margin: '30px 10px'
+}
+
+const title = {
+    margin: "0 auto 0 35px",
+}
+
+const font = {
+    fontSize: "30px",
+    color: "gray"
+}
+
 
 const Header = () => {
     return (
         <div style={outline}>
-            sfsgfa
+            <div style={sameLine}>
+                <div style={title}>
+                    <strong style={font}>ReactForm</strong>
+                </div>
+            </div>
+            <div style={headerSame}>
+                <div style={right2}>
+                    <Button variant="contained" href="../" color="success">TopPage</Button>
+                </div>
+                <div style={right2}>
+                    <Button variant="contained" href="../Form" color="secondary">FormPage</Button>
+                </div>
+                <div style={right2}>
+                    <Button variant="contained" href="../Question" color="secondary">QuestionIndex</Button>
+                </div>
+            </div>
         </div>
     )
 }
