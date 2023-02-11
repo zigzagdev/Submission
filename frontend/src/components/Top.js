a22b0576da6fcf6773152df2d2beed2eb5b844c8
zigzagdev <msts.oo0131@gmail.com>
2023-02-11 18:40:27 +0900

    topPage_fixResponsive

--- frontend/src/components/Top.js
+++ frontend/src/components/Top.js
@@ -1,17 +1,74 @@
 import React from 'react';
+import {Card} from "@mui/material";
 
 const main = {
-    display: "flex",
     flexDirection: "column",
     height: "100%",
     margin: 0,
     padding: 0
 }
+
+const opinion = {
+    margin: "4% 12%",
+    flexWrap: "wrap",
+}
+
+const totalOpinion = {
+    display: "flex",
+    margin: "2% 3%",
+    flexWrap: "wrap",
+    justifyContent: "spaceEvenly",
+}
+
+const eachOpinion = {
+    margin: "4% 4%",
+}
+
+const eachCard = {
+    color: "snow",
+    width: "250px",
+    height: "170px",
+    borderRadius: "20px",
+}
+
 const Top = () => {
-    console.log('here')
     return (
         <div style={main}>
+            <div style={opinion}>
+                <div style={totalOpinion}>
+                    <div style={eachOpinion}>
+                        <Card style={eachCard}>aa</Card>
+                    </div>
+                    <div style={eachOpinion}>
+                        <Card style={eachCard}>aa</Card>
+                    </div>
+                    <div style={eachOpinion}>
+                        <Card style={eachCard}>aa</Card>
+                    </div>
+                    <div style={eachOpinion}>
+                        <Card style={eachCard}>aa</Card>
+                    </div>
+                    <div style={eachOpinion}>
+                        <Card style={eachCard}>aa</Card>
+                    </div>
+                    <div style={eachOpinion}>
+                        <Card style={eachCard}>aa</Card>
+                    </div>
+                    <div style={eachOpinion}>
+                        <Card style={eachCard}>aa</Card>
+                    </div>
+                    <div style={eachOpinion}>
+                        <Card style={eachCard}>aa</Card>
+                    </div>
+                    <div style={eachOpinion}>
+                        <Card style={eachCard}>aa</Card>
+                    </div>
+                    <div style={eachOpinion}>
+                        <Card style={eachCard}>aa</Card>
+                    </div>
 
+                </div>
+            </div>
         </div>
     );
 };
