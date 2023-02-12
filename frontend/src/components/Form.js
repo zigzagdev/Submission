2792d7bea4ec630d3bdfc6c8c29fb92b9fdb5653
zigzagdev <msts.oo0131@gmail.com>
2023-02-12 18:53:15 +0900

    insertMysql_Complete

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,19 +1,31 @@
 import React, {useState,} from 'react';
 import {useForm} from 'react-hook-form';
-import {useStepContext} from "@mui/material";
+import axios from "axios";
 
 const Form = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState("")
+    const [error,setError] = useState(false)
 
-    const handleSubmit = (e) => {
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log({
+        console.log(
             email,
-            password,
             name
-        });
+        )
+        try {
+          await  axios.post("http://localhost:3003/Form", {
+              name: name,
+              email: email,
+              password: password
+          })
+            console.log('suc')
+        } catch (err) {
+            console.log(err);
+            setError(true)
+        }
     };
 
     const handleChangeEmail = (e) => {
@@ -26,12 +38,18 @@ const Form = () => {
         setName(e.target.value);
     };
     return (
-        <div className="App">
-            <h1>Form</h1>
-            <form onSubmit={handleSubmit}>
+        <div className="">
+            <h1 style={{display: "inline-block", margin: "5% 16%"}}>Form</h1>
+            <form onSubmit={handleSubmit} style={{ marginLeft: "28%"}}>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input id="name" name="name" value={name} onChange={handleChangeName}/>
+                    {error && "here wrong"}
+                </div>
                 <div>
                     <label htmlFor="email">Email</label>
                     <input id="email" name="email" value={email} onChange={handleChangeEmail}/>
+                    {error && "here wrong"}
                 </div>
                 <div>
                     <label htmlFor="password">パスワード</label>
@@ -42,10 +60,7 @@ const Form = () => {
                         onChange={handleChangePassword}
                         type="password"
                     />
-                </div>
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <input id="name" name="name" value={name} onChange={handleChangeName}/>
+                    {error && "here wrong"}
                 </div>
                 <div>
                     <button type="submit">Send</button>
