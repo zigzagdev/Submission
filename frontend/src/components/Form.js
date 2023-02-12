db4cb15dd8129781533cdb3614fff57d414df655
zigzagdev <msts.oo0131@gmail.com>
2023-02-12 14:42:21 +0900

    makeBaseModelForm

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,18 +1,18 @@
-import React, { useState, } from 'react';
-import { useForm } from 'react-hook-form';
+import React, {useState,} from 'react';
+import {useForm} from 'react-hook-form';
+import {useStepContext} from "@mui/material";
 
 const Form = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [name, setName] = useState('');
-    const { register, formState: {errors} } = useForm();
+    const [name, setName] = useState("")
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log({
             email,
             password,
-            name,
+            name
         });
     };
 
@@ -26,17 +26,12 @@ const Form = () => {
         setName(e.target.value);
     };
     return (
-        <div className="form">
+        <div className="App">
+            <h1>Form</h1>
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input id="email" name="email" value={email}
-                           onChange={handleChangeEmail} {...register('email', {required: true})}/>
-                    {errors.email && <div>入力が必須の項目です</div>}
-                </div>
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <input id="name" name="name" value={name} onChange={handleChangeName}/>
+                    <input id="email" name="email" value={email} onChange={handleChangeEmail}/>
                 </div>
                 <div>
                     <label htmlFor="password">パスワード</label>
@@ -49,7 +44,11 @@ const Form = () => {
                     />
                 </div>
                 <div>
-                    <button type="submit">ログイン</button>
+                    <label htmlFor="name">Name</label>
+                    <input id="name" name="name" value={name} onChange={handleChangeName}/>
+                </div>
+                <div>
+                    <button type="submit">Send</button>
                 </div>
             </form>
         </div>
