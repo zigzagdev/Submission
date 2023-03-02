13e71de8d34bcd32b913c9ab06e046caf2938667
zigzagdev <msts.oo0131@gmail.com>
2023-03-03 01:21:06 +0900

    fixPath

--- frontend/src/main/Main.js
+++ frontend/src/main/Main.js
@@ -19,7 +19,7 @@ const Main = () => {
                     <Route path='/Update/:id' element={<Update/>}/>
                     <Route path='/' element={<Pagination/>}/>
                     <Route path='/Page/:page' element={<Page/>}/>
-                    <Route path='/Page/:id/:id' element={<Each/>}/>
+                    <Route path='/Page/:page/:id' element={<Each/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
