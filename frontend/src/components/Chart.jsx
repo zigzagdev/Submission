efd1c5335b673d67284fe39356b04f47ab891e9b
zigzagdev <msts.oo0131@gmail.com>
2023-03-05 23:43:47 +0900

    chartAPI_add

--- frontend/src/components/Chart.jsx
+++ frontend/src/components/Chart.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import React, {useEffect, useState} from 'react';
+import {Bar} from 'react-chartjs-2';
 import 'chart.js/auto';
+import axios from "axios";
+import each from "./Each";
 
 const Chart = () => {
-    const data = {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        datasets: [
-            {
-                label: 'Registered Records here.',
-                data: [4, 25, 1, 23, 55, 33, 10, 23, 19, 55, 44, 8],
-                backgroundColor: ['Red', 'Blue', 'Purple', 'Yellow', 'Green', 'Pink', 'Black',
-                    'Gray', 'Orange', 'Brown', 'DarkBlue', 'LightGreen'
-                ],
-            }
-        ]
-    }
+    const [apiData, setApiData] = useState([]);
+    let apiFetch = [];
+    let eachMonth = [];
+    useEffect(() => {
+        const fetchAPI = async () => {
+            const res = await axios.get(`http://localhost:3003`);
+            res.data.map(function (value) {
+                let date = new Date(value.updated_at)
+                console.log(date.getFullYear())
+                apiFetch.push(date.getMonth() + 1)
+            });
+            let eachMonth = apiFetch.reduce(function (prev, current) {
+                prev[current] = (prev[current] || 0) + 1;
+                return prev;
+            }, []);
+
+            setApiData({
+                    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                    datasets: [
+                        {
+                            label: 'Registered Records here.',
+                            labelColors: 'black',
+                            data: [333, eachMonth[2], eachMonth[3], eachMonth[4], eachMonth[5], eachMonth[6]
+                                , eachMonth[7], eachMonth[8], eachMonth[9], eachMonth[10], eachMonth[11], eachMonth[12]
+                            ],
+                            backgroundColor: ['Red', 'Blue', 'Purple', 'Yellow', 'Green', 'Pink', 'Black',
+                                'Gray', 'Orange', 'Brown', 'DarkBlue', 'LightGreen'],
+                        }
+                    ]
+                },
+            );
+        };
+        fetchAPI()
+    }, []);
+    console.log(apiData)
     return (
         <div>
-            <Bar data={data} style={{margin: '7% 20% 5% 20%'}}></Bar>
+            <Bar data={apiData}/>
         </div>
     );
 };
