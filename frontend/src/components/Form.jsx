65c6de0587f2ade0729475cbcd6b380171f8aa7a
zigzagdev <msts.oo0131@gmail.com>
2023-03-12 18:02:17 +0900

    W.I.P_responsiveValidation

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,7 +2,8 @@ import React, {useState,} from 'react';
 import axios from "axios";
 import {Button} from "@mui/material";
 import {useNavigate} from "react-router-dom";
-import {sha256} from 'crypto-hash';
+import bcrypt from 'bcryptjs';
+import {useFormik} from "formik";
 
 const form = {
     margin: " 5% ",
@@ -10,154 +11,144 @@ const form = {
 }
 
 const inputs = {
-    margin: "0 50%",
-    width: "180%",
+    margin: "0 20px",
+    width: "300px",
     height: "30px"
 }
 
 const errorInputs = {
-    margin: "0 50%",
-    width: "180%",
-    height: "30px",
-    color: "red"
+    color: "red",
+    flex: 1
 }
 
 const opinions = {
-    width: "180%",
-    margin: "0 50%",
+    width: "300px",
+    margin: "0 20px",
     height: "100px"
 }
 
-const Form = () => {
-    const [email, setEmail] = useState("");
-    const [correctEmail, setCorrectEmail] = useState(false);
-    const [password, setPassword] = useState("");
-    const [correctPass, setCorrectPass] = useState(false);
-    const [name, setName] = useState("");
-    const [correctName, setCorrectName] = useState(false);
-    const [opinion, setOpinion] = useState("");
-    const [correctOpinion, setCorrectOpinion] = useState(false);
-    const inputHandler = (e) => {
-        setOpinion(e.target.value);
-    };
-    const [error, setError] = useState(false);
-    const UseNav = useNavigate();
-    const hashedPassword = sha256(password);
-    console.log(password)
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (name.length < 5 || name.length > 100) {
-            setCorrectName(true);
-            return;
-        }
-        if (!/^[a-zA-Z]+$/.test(name)) {
-            setCorrectName(true);
-            return;
-        }
-        if (opinion.length < 5 || opinion.length > 1000) {
-            setCorrectOpinion(true);
-            return;
-        }
-        if (password.length < 8 || password.length >30) {
-            setCorrectPass(true)
-            return;
-        }
-        try {
-            await axios.post("http://localhost:3003/Form", {
-                    name: name,
-                    email: email,
-                    password: hashedPassword,
-                    opinion: opinion
-                }
-            )
-        } catch (error) {
-            setError(true)
-        }
-    };
+const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+    opinion: ''
+}
 
-    const handleChangeEmail = (e) => {
-        setEmail(e.target.value);
-    };
-    const handleChangePassword = (e) => {
-        setPassword(e.target.value);
-    };
-    const handleChangeName = (e) => {
-        setName(e.target.value);
-    };
-    const handleChangeOpinion = (e) => {
-        setOpinion(e.target.value);
-    };
+const onSubmit = (values) => {
+    console.log(values)
+    // try {
+    //     axios({
+    //         url: "http://localhost:3003/Form",
+    //         method: "post",
+    //         data: values,
+    //     })
+    // } catch (error) {
+    //     console.log('errors');
+    // }
+}
+
+const validate = values => {
+    console.log(values.name)
+    const errors = {};
+
+    if (!values.name) {
+        errors.name = 'Name must be required !!'
+    } else if (!/^[a-zA-Z]+$/.test(values.name)) {
+        errors.name = 'Name must be in alphabet !!'
+    }
+
+    if (!values.email) {
+        errors.email = 'Your email must be required !!'
+    }
+
+    if (!values.password) {
+        errors.password = 'Password must be required !!'
+    } else if (values.password.length < 8 || values.password.length > 1000) {
+        errors.password = 'Password must be filled in 8 to 1000 characters .'
+    }
+
+    if (!values.opinion) {
+        errors.opinion = 'Your opinion  must be required !!'
+    } else if (values.opinion.length < 5 || values.opinion.length > 10000) {
+        errors.opinion = 'Opinion must be filled in 5 to 10000 characters .'
+    }
+
+    return errors;
+}
+
+const Form = () => {
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate
+    });
     return (
         <div className="Form">
             <h1 style={{display: "inline-block", margin: "5% 20% 2% 25%"}}>Submission</h1>
-            <div style={{border: "3px solid #000000", margin: "0 30%", borderRadius: "5%"}}>
+            <div style={{border: "3px solid #000000", margin: "0 300px", borderRadius: "5%"}}>
                 <div style={form}>
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={formik.handleSubmit}>
                         <div style={{margin: "5% 0", display: "flex"}}>
-                            <div style={{width: "30%"}}>
+                            <div style={{width: "90px"}}>
                                 <label htmlFor="name">Name:</label>
                             </div>
                             <div style={{flex: 1}}>
                                 <input
                                     id="name"
                                     name="name"
-                                    value={name}
-                                    onChange={handleChangeName}
                                     style={inputs}
-                                    required={error}
+                                    onChange={formik.handleChange}
+                                    value={formik.values.name}
                                 />
-                                <div style={errorInputs}>{correctName && "here wrong"}</div>
+                                <div style={errorInputs}>{formik.errors.name}</div>
                             </div>
                         </div>
                         <div style={{margin: "5% 0", display: "flex"}}>
-                            <div style={{width: "30%"}}>
+                            <div style={{width: "90px"}}>
                                 <label htmlFor="email">Email:</label>
                             </div>
                             <div style={{flex: 1}}>
                                 <input id="email"
                                        name="email"
-                                       value={email}
-                                       onChange={handleChangeEmail}
                                        style={inputs}
-                                       required={true}
+                                       onChange={formik.handleChange}
+                                       value={formik.values.email}
                                 />
-                                <div style={errorInputs}>{correctEmail && "here wrong"}</div>
+                                <div style={errorInputs}>{formik.errors.email}</div>
                             </div>
                         </div>
                         <div style={{margin: "5% 0", display: "flex"}}>
-                            <div style={{width: "30%"}}>
+                            <div style={{width: "90px"}}>
                                 <label htmlFor="password">Password:</label>
                             </div>
                             <div style={{flex: 1}}>
                                 <input
                                     id="password"
                                     name="password"
-                                    value={password}
-                                    onChange={handleChangePassword}
                                     type="password"
                                     style={inputs}
-                                    required={true}
+                                    onChange={formik.handleChange}
+                                    value={formik.values.password}
                                 />
-                                <div style={errorInputs}>{correctPass && "here wrong"}</div>
+                                <div style={errorInputs}>{formik.errors.password}</div>
                             </div>
                         </div>
                         <div style={{margin: "5% 0", display: "flex"}}>
-                            <div style={{width: "30%"}}>
+                            <div style={{width: "90px"}}>
                                 <label htmlFor="opinion">Opinion:</label>
                             </div>
                             <div style={{flex: 1}}>
                             <textarea id="opinion"
                                       name="opinion"
-                                      value={opinion}
-                                      onChange={inputHandler}
                                       style={opinions}
-                                      required={true}
+                                      onChange={formik.handleChange}
+                                      value={formik.values.opinion}
                             />
-                                <div style={errorInputs}>{correctOpinion && "here wrong"}</div>
+                                <div style={errorInputs}>{formik.errors.opinion}</div>
                             </div>
                         </div>
-                        <div style={{display: "inline-block", margin: "5% 100%"}}>
-                            <Button onClick={handleSubmit} variant="contained" sx={"background-color: lightblue;"}>
+                        <div style={{ marginLeft: "210px"}}>
+                            <Button variant="contained" sx={"background-color: lightblue;"} type="submit">
                                 <strong style={{color: "black"}}>Send</strong>
                             </Button>
                         </div>
